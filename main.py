855b62473f93835f5f1b8e3a962636c552f59129
erennew <erenyeagermikasa84@gmail.com>
2025-04-06 18:58:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import asyncio
 from bot import Bot
 
-bot = Bot()
-
-loop = asyncio.get_event_loop()
-
 if __name__ == "__main__":
-    try:
-        loop.run_until_complete(bot.start())
-    except (KeyboardInterrupt, SystemExit):
-        print("Bot stopped.")
-    finally:
-        loop.run_until_complete(bot.stop())
+    Bot().run()
